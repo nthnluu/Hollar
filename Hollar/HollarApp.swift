5eb270da83815655bf3f35c091afb65a5cd20103
nathanluu <nate1299@me.com>
2020-10-03 11:42:52 -0700

    Initial Commit

---
+++ Hollar/HollarApp.swift
@@ -0,0 +1,17 @@
+//
+//  HollarApp.swift
+//  Hollar
+//
+//  Created by Nathan Luu on 10/3/20.
+//
+
+import SwiftUI
+
+@main
+struct HollarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
